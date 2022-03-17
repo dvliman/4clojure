8bd842bf87cdad0e71154796e0eab4a213ccacc7
David Liman <limanoit@gmail.com>
2022-03-17 13:07:53 -0700

    first commit

---
+++ test/dvliman/forclojure_test.clj
@@ -0,0 +1,7 @@
+(ns dvliman.forclojure-test
+  (:require [clojure.test :refer :all]
+            [dvliman.forclojure :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
