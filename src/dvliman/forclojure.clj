8ecab1c51f81475c25098e84b1dd41097e60ba77
David Liman <limanoit@gmail.com>
2022-03-17 21:10:19 -0700

    decurry

--- src/dvliman/forclojure.clj
+++ src/dvliman/forclojure.clj
@@ -1,6 +1,38 @@
 (ns dvliman.forclojure
-  (:gen-class))
+  (:require [clojure.string :as str]))
 
-(defn -main
+(defn generate-palindromes [end]
+  (filter (fn [x]
+            (and (= (str x) (str/join "" (reverse (str x))))
+                 (>= x end))) (range)))
+
+(defn create-palindrome [input b is-odd]
+  (loop [n (if (= is-odd 1) (/ input b) input)
+         palin input]
+    (if (> n 0)
+      (recur (/ n b) (+ (* palin b) (mod n b)))
+      palin)))
+
+(defn problem-153 [ss]
+  (if (seq (reduce (fn [acc s]
+                     (if (seq (clojure.set/intersection acc s))
+                       (reduced #{})
+                       (clojure.set/union acc s)))
+                   #{} ss))
+    true
+    false))
+
+(defn problem-156 [default ks]
+  (zipmap ks (repeat (count ks) default)))
+
+(defn problem-157 [coll]
+  (map-indexed (fn [index item]
+                 [item index]) coll))
+
+(defn problem-158 [f]
+  (fn [& args]
+    (reduce #(%1 %2) f args)))
+
+#_(defn -main
   [& args]
   (println "hello world"))
--- test/dvliman/forclojure_test.clj
+++ test/dvliman/forclojure_test.clj
@@ -1,7 +1,72 @@
 (ns dvliman.forclojure-test
   (:require [clojure.test :refer :all]
-            [dvliman.forclojure :refer :all]))
+            [dvliman.forclojure :as src]))
 
-(deftest a-test
-  (testing "fixed"
-    (is (= 1 1))))
+(deftest problem-153-pairwise-disjoint-sets
+    (is (= (src/problem-153 #{#{\U} #{\s} #{\e \R \E} #{\P \L} #{\.}})
+           true))
+    (is (= (src/problem-153 #{#{:a :b :c :d :e}
+                             #{:a :b :c :d}
+                             #{:a :b :c}
+                             #{:a :b}
+                             #{:a}})
+           false))
+    (is (= (src/problem-153 #{#{[1 2 3] [4 5]}
+                             #{[1 2] [3 4 5]}
+                             #{[1] [2] 3 4 5}
+                             #{1 2 [3 4] [5]}})
+           true))
+    (is (= (src/problem-153 #{#{'a 'b}
+                             #{'c 'd 'e}
+                             #{'f 'g 'h 'i}
+                             #{''a ''c ''f}})
+           true))
+    (is (= (src/problem-153 #{#{'(:x :y :z) '(:x :y) '(:z) '()}
+                             #{#{:x :y :z} #{:x :y} #{:z} #{}}
+                             #{'[:x :y :z] [:x :y] [:z] [] {}}})
+           false))
+    (is (= (src/problem-153 #{#{(= "true") false}
+                             #{:yes :no}
+                             #{(class 1) 0}
+                             #{(symbol "true") 'false}
+                             #{(keyword "yes") ::no}
+                             #{(class '1) (int \0)}})
+           false))
+    (is (= (src/problem-153 (set [(set [distinct?])
+                                 (set [#(-> %) #(-> %)])
+                                 (set [#(-> %) #(-> %) #(-> %)])
+                                 (set [#(-> %) #(-> %) #(-> %)])]))
+           true))
+    (is (= (src/problem-153 #{#{(#(-> *)) + (quote mapcat) #_ nil}
+                             #{'+ '* mapcat (comment mapcat)}
+                             #{(do) set contains? nil?}
+                             #{, , , #_, , empty?}})
+                       false)))
+
+(deftest problem-156-map-defaults
+  (is (= (src/problem-156 0 [:a :b :c]) {:a 0 :b 0 :c 0}))
+  (is (= (src/problem-156 "x" [1 2 3]) {1 "x" 2 "x" 3 "x"}))
+  (is (= (src/problem-156 [:a :b] [:foo :bar]) {:foo [:a :b] :bar [:a :b]})))
+
+(deftest problem-157-indexing-sequences
+  (is (= (src/problem-157 [:a :b :c]) [[:a 0] [:b 1] [:c 2]]))
+  (is (= (src/problem-157 [0 1 3]) '((0 0) (1 1) (3 2))))
+  (is (= (src/problem-157 [[:foo] {:bar :baz}]) [[[:foo] 0] [{:bar :baz} 1]])))
+
+(deftest problem-158-decurry
+  (is (= 10 ((src/problem-158 (fn [a]
+                                (fn [b]
+                                  (fn [c]
+                                    (fn [d]
+                                      (+ a b c d))))))
+             1 2 3 4)))
+  (is (= 24 ((src/problem-158 (fn [a]
+                                (fn [b]
+                                  (fn [c]
+                                    (fn [d]
+                                      (* a b c d))))))
+             1 2 3 4)))
+  (is (= 25 ((src/problem-158 (fn [a]
+                                (fn [b]
+                                  (* a b))))
+                          5 5))))
